4373a150ba97d0465e9885dd6115281b88d5c05a
brutfix <stanley_ic@outlook.com>
2023-03-16 09:57:33 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 int main()
+/**
+ * main - to print a line with a puts function
+ * return 0 which mean success
 {
 	puts("Programming is like building a multilingual puzzle\n");
 	return 0;
