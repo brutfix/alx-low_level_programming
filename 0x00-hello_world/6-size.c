386ccc42d3bfed42e4e3324f70cb93bc14506fdd
brutfix <stanley_ic@outlook.com>
2023-03-16 13:21:14 -0700

    all commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - A program that prints the size of various computer types
  * Return: 0 (Success)
  */
-int main(void)
+int main(:void)
 {
 char a;
 int b;
