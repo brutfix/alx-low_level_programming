6708e03c7f7f50d5757a07a5d8330cafeeea6c59
brutfix <stanley_ic@outlook.com>
2023-03-17 09:30:45 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+/**
+ *main - a program that print out lower case alphabeth
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+	putchar(c);
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
