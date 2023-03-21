2163d44163e0aa5d3ce288dda44f10ba469f410d
brutfix <stanley_ic@outlook.com>
2023-03-21 11:15:36 -0700

    update

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,23 +5,22 @@
  */
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2;
-
-	while (i < 50)
-	{
-	if (i == 0)
-	printf("%ld", j);
-	else if (i == 1)
-	printf(", %ld", k);
-	else
-	{
-	k += j;
-	j = k - j;
-	printf(", %ld", k);
-	}
-	++i;
-	}
-	print("\n");
-	return (0);
+int i = 0;
+long j = 1, k = 2;
+while (i < 50)
+{
+if (i == 0)
+printf("%ld", j);
+else if (i == 1)
+printf(", %ld", k);
+else
+{
+k += j;
+j = k - j;
+printf(", %ld", k);
+}
+++i;
+}
+print("\n");
+return (0);
 }
