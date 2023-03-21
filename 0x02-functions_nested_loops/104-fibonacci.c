7969584badeefb5b53ccfdfb76e51af1b7eb0597
brutfix <stanley_ic@outlook.com>
2023-03-21 11:58:26 -0700

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -18,10 +18,10 @@ int main(void)
 	fib1 = fib2;
 	fib2 = sum;
 	}
-	fib1_half = fib1 / 10000000000;
-	fib2_half = fib2 / 10000000000;
-	fib1_half = fib1 % 10000000000;
-	fib2_half = fib2 % 10000000000;
+	fib1_half1 = fib1 / 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
+	fib2_half2 = fib2 % 10000000000;
 	for (count = 93; count < 99; count++)
 	{
 	half1 = fib1_half1 + fib2_half1;
@@ -33,7 +33,7 @@ int main(void)
 	}
 	printf("%lu%lu", half1, half2);
 	if (count != 98)
-	prinntf(", ");
+	printf(", ");
 	fib1_half1 = fib2_half1;
 	fib1_half2 = fib2_half2;
 	fib2_half1 = half1;
