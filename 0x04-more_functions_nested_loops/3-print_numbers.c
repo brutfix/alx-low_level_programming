84d829c25f0b6dfa47a9f6fc6ddbbc21ecb3b405
brutfix <stanley_ic@outlook.com>
2023-03-23 15:05:46 -0700

    update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - print the number from 0 to 9
+* Return: The number from 0 up to 9
+*/
+
+void print_numbers(void)
+{
+	int x;
+	
+	for (x = 0; x <= 0; x++)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
