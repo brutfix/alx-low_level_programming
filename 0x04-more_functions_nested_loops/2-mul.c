c99249b50999549429d26857ead17f77a731e7f4
brutfix <stanley_ic@outlook.com>
2023-03-23 14:49:49 -0700

    update 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* mul - mulutiples of two integers
+* @x: the number first number to multiply
+* @y: the seconfd number to multiply with the first number
+* Return: The result of the multiplication between x person and y person
+*/
+
+int mul(int x, int y)
+{
+	return (x + y);
+}
