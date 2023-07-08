03f46a3e62f5ba11c84285bc21adbf435d7853ea
brutfix <stanley_ic@outlook.com>
2023-07-08 04:39:39 -0700

    commit 1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int a;
+	unsigned int cov_num = 0;
+
+	if (!b)
+		return (0);
+
+	for (a = 0; b[a]; a++)
+	{
+		if (b[a] < '0' || b[a] > '1')
+			return (0);
+		cov_num = 2 * cov_num + (b[a] - '0');
+	}
+
+	return (cov_num);
+}
