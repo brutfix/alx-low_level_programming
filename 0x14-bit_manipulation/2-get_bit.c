3e89f8c57c9b90a2b1eb8f70a94090a722824045
brutfix <stanley_ic@outlook.com>
2023-07-08 04:59:44 -0700

    commit 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at an index in a decimal number
+ * @n: number to search
+ * @index: index of the bit
+ *
+ * Return: value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int val_n;
+
+	if (index > 63)
+		return (-1);
+
+	val_n = (n >> index) & 1;
+
+	return (val_n);
+}
